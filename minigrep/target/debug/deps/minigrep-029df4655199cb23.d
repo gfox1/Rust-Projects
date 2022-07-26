5c66a89f62ca59fc230e198c89798e3932460ac5
Graeme Fox <ac.graeme@gmail.com>
2022-07-26 20:48:33 +0100

    finished program

---
+++ minigrep/target/debug/deps/minigrep-029df4655199cb23.d
@@ -0,0 +1,5 @@
+/home/acgra/rust/Rust Projects/minigrep/target/debug/deps/minigrep-029df4655199cb23: src/main.rs
+
+/home/acgra/rust/Rust Projects/minigrep/target/debug/deps/minigrep-029df4655199cb23.d: src/main.rs
+
+src/main.rs:
